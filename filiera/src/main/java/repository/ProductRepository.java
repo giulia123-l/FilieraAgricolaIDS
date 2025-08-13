9369e3661b9e3da67dd4b5896062335e91fd1988
giulia123-l <giulia.lupi@studenti.unicam.it>
2025-08-13 17:34:16 +0200

    aggiunta repository
    aggiunta cartella repository con ProductRepository e UserRepository

---
+++ filiera/src/main/java/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package repository;
+import com.example.filiera.domain.Prodotto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+public interface ProductRepository extends JpaRepository<Prodotto, Long> {}
